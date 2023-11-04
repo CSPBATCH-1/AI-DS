e172533ede0acc8d1085ae65a327f1c99841161e
CSPBATCH-1 <149890935+CSPBATCH-1@users.noreply.github.com>
2023-11-04 22:42:54 +0530

    ok

---
+++ pds.js
@@ -0,0 +1,32 @@
+const galleryImage = document.getElementById("gallery-image");
+
+const images = [
+  "pds6.jpg",
+  "pds7.jpg",
+  "pds8.jpg",
+  "pds9.jpg",
+  "pds10.jpg",
+  "pds11.jpg",
+  "pds12.jpg",
+  "pds13.jpg",
+  "pds14.jpg",
+];
+
+let currentIndex = 0;
+
+function showImage(index) {
+  galleryImage.src = images[index];
+  currentIndex = index;
+}
+
+function showNextImage() {
+  currentIndex = (currentIndex + 1) % images.length;
+  showImage(currentIndex);
+}
+
+function startSlideshow() {
+  showImage(currentIndex);
+  setInterval(showNextImage, 4000); // Change image every 3 seconds (3000 milliseconds)
+}
+
+startSlideshow();
